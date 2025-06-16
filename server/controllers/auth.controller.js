a325e80705f75bb77837f65696d260f1a07d5e8e
PratikKrGhosh <work.pratikkrghosh@gmail.com>
2025-06-17 00:42:03 +0530

    added basic log in functionality

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import { createUser } from "../services/user.service.js";
+import { createUser, getUserByUserName } from "../services/user.service.js";
 
 export const renderSignInPage = (req, res) => {
   try {
@@ -16,11 +16,19 @@ export const renderRegisterPage = (req, res) => {
   }
 };
 
-export const signin = (req, res) => {
+export const signin = async (req, res) => {
   try {
-    res.status(200).render("register");
+    const { userName, password } = req.body;
+    const [user] = await getUserByUserName(userName);
+
+    if (!user) return res.status(404).send("User Not Found");
+
+    if (user.password !== password)
+      return res.status(400).send("Wrong Password");
+
+    res.status(200).redirect("/");
   } catch (err) {
-    return res.status(404).send("Page not Found");
+    return res.status(404).send("Something went wrong");
   }
 };
 
@@ -30,10 +38,10 @@ export const register = async (req, res) => {
 
     const [user] = await createUser({ userName, name, email, password });
 
-    if (!user) return res.status(400).send("Something Went Wrong");
+    if (!user) return res.status(400).send("Could not create User!");
 
     return res.status(200).redirect("/sign");
   } catch (err) {
-    return res.status(404).send("Page not Found");
+    return res.status(404).send("Something Went Wrong");
   }
 };
--- server/services/user.service.js
+++ server/services/user.service.js
@@ -1,3 +1,4 @@
+import { eq } from "drizzle-orm";
 import db from "../config/db.js";
 import { usersTable } from "../drizzle/schema.js";
 
@@ -6,6 +7,17 @@ export const createUser = async ({ userName, name, email, password }) => {
     return await db
       .insert(usersTable)
       .values({ userName, name, email, password });
+  } catch (err) {
+    return null;
+  }
+};
+
+export const getUserByUserName = async (userName) => {
+  try {
+    return await db
+      .select()
+      .from(usersTable)
+      .where(eq(usersTable.userName, userName));
   } catch (err) {
     return null;
   }
